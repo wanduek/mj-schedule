e2824b9231ebd30849ea30dea09987c959b2d3f9
wanduek <minjoois2@gmail.com>
2024-08-22 17:45:49 +0900

    init commit

---
+++ src/main/java/com/sparta/scheduleserver/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.sparta.scheduleserver.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final Userservice userservice;
+
+
+
+}
